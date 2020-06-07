b8c2b9cfde5cc82f69614adbd26aa3587a99f53f
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-06-07 14:26:38 +0200

    use username as primary key for a user

--- app/src/main/java/com/example/diappetes/persistence/model/User.java
+++ app/src/main/java/com/example/diappetes/persistence/model/User.java
@@ -1,17 +1,26 @@
 package com.example.diappetes.persistence.model;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class User {
+
+    @NonNull
     @PrimaryKey
-    public int uid;
+    public String uid;
 
     @ColumnInfo(name = "email")
     public String email;
 
     @ColumnInfo(name = "password")
     public String password;
+
+    public User(@NonNull String uid, String email, String password) {
+        this.uid = uid;
+        this.email = email;
+        this.password = password;
+    }
 }
