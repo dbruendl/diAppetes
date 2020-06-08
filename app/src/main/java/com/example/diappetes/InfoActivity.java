f571e358f290d52e72eacb50fe67bcd25922b574
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-06-08 20:49:54 +0200

    code format

--- app/src/main/java/com/example/diappetes/InfoActivity.java
+++ app/src/main/java/com/example/diappetes/InfoActivity.java
@@ -2,7 +2,6 @@ package com.example.diappetes;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,7 +15,6 @@ import com.example.diappetes.info.UserListAdapter;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Calendar;
 
 import javax.inject.Inject;
@@ -36,6 +34,7 @@ public class InfoActivity extends DaggerAppCompatActivity {
     ViewModelProviderFactory viewModelProviderFactory;
 
     private InfoViewModel infoViewModel;
+    private Disposable findAllUsersDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +49,7 @@ public class InfoActivity extends DaggerAppCompatActivity {
         userLayoutManager = new LinearLayoutManager(this);
         activityInfoBinding.userRecyclerView.setLayoutManager(userLayoutManager);
 
-        Disposable disposable = infoViewModel.findAll()
+        findAllUsersDisposable = infoViewModel.findAll()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(users -> {
@@ -67,30 +66,30 @@ public class InfoActivity extends DaggerAppCompatActivity {
         //TIME
 
         Button homebtn2 = findViewById(R.id.homebtn);
-        homebtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent starthomeintent2 = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(starthomeintent2);
-            }
+        homebtn2.setOnClickListener(v -> {
+            Intent starthomeintent2 = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(starthomeintent2);
         });
 
         Button petbtn2 = findViewById(R.id.petbtn);
-        petbtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startpetintent2 = new Intent(getApplicationContext(), PetActivity.class);
-                startActivity(startpetintent2);
-            }
+        petbtn2.setOnClickListener(v -> {
+            Intent startpetintent2 = new Intent(getApplicationContext(), PetActivity.class);
+            startActivity(startpetintent2);
         });
 
         Button statbtn2 = findViewById(R.id.statbtn);
-        statbtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startstatintent2 = new Intent(getApplicationContext(), StatActivity.class);
-                startActivity(startstatintent2);
-            }
+        statbtn2.setOnClickListener(v -> {
+            Intent startstatintent2 = new Intent(getApplicationContext(), StatActivity.class);
+            startActivity(startstatintent2);
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(findAllUsersDisposable != null) {
+            findAllUsersDisposable.dispose();
+        }
     }
 }
