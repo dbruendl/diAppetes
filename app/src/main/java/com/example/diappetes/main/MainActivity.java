6892f164d3e57fc09d853f9ffd4b49ed51acb8e5
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-06-11 20:59:59 +0200

    update app theme to use material design theme

--- app/src/main/java/com/example/diappetes/main/MainActivity.java
+++ app/src/main/java/com/example/diappetes/main/MainActivity.java
@@ -10,12 +10,7 @@ import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
