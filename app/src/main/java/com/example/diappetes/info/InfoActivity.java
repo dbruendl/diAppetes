a60c52f47212d54068a819fc96816cc9b85d982a
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-06-11 22:03:24 +0200

    add cards for information

--- app/src/main/java/com/example/diappetes/info/InfoActivity.java
+++ app/src/main/java/com/example/diappetes/info/InfoActivity.java
@@ -2,43 +2,24 @@ package com.example.diappetes.info;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.diappetes.main.MainActivity;
 import com.example.diappetes.PetActivity;
-import com.example.diappetes.R;
 import com.example.diappetes.StatActivity;
 import com.example.diappetes.databinding.ActivityInfoBinding;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import com.example.diappetes.main.MainActivity;
 
 import javax.inject.Inject;
 
-import dagger.android.support.DaggerAppCompatActivity;
 import dagger.hilt.android.AndroidEntryPoint;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 @AndroidEntryPoint
 public class InfoActivity extends AppCompatActivity {
     @Inject
     public InfoViewModel infoViewModel;
 
-    RecyclerView.Adapter userAdapter;
-    RecyclerView.LayoutManager userLayoutManager;
-
     ActivityInfoBinding activityInfoBinding;
-
-    private Disposable findAllUsersDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,52 +27,19 @@ public class InfoActivity extends AppCompatActivity {
         activityInfoBinding = ActivityInfoBinding.inflate(getLayoutInflater());
         setContentView(activityInfoBinding.getRoot());
 
-        activityInfoBinding.userRecyclerView.setHasFixedSize(true);
-
-        userLayoutManager = new LinearLayoutManager(this);
-        activityInfoBinding.userRecyclerView.setLayoutManager(userLayoutManager);
-
-        findAllUsersDisposable = infoViewModel.findAll()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(users -> {
-                    userAdapter = new UserListAdapter(users);
-                    activityInfoBinding.userRecyclerView.setAdapter(userAdapter);
-                });
-
-        //TIME
-        DateFormat datetime = new SimpleDateFormat("yyyy MM HH:mm:ss");
-        String date = datetime.format(Calendar.getInstance().getTime());
-
-        TextView timetxt = findViewById(R.id.timetxt);
-        timetxt.setText(date);
-        //TIME
-
-        Button homebtn2 = findViewById(R.id.homebtn);
-        homebtn2.setOnClickListener(v -> {
+        activityInfoBinding.homebtn.setOnClickListener(v -> {
             Intent starthomeintent2 = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(starthomeintent2);
         });
 
-        Button petbtn2 = findViewById(R.id.petbtn);
-        petbtn2.setOnClickListener(v -> {
+        activityInfoBinding.petbtn.setOnClickListener(v -> {
             Intent startpetintent2 = new Intent(getApplicationContext(), PetActivity.class);
             startActivity(startpetintent2);
         });
 
-        Button statbtn2 = findViewById(R.id.statbtn);
-        statbtn2.setOnClickListener(v -> {
+        activityInfoBinding.statbtn.setOnClickListener(v -> {
             Intent startstatintent2 = new Intent(getApplicationContext(), StatActivity.class);
             startActivity(startstatintent2);
         });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if(findAllUsersDisposable != null) {
-            findAllUsersDisposable.dispose();
-        }
     }
 }
